9da38a6c9ae17785d5782b5be89c9b406b29118b
Bowie <alexfperdigao@gmail.com>
2024-02-21 10:52:54 -0300

    projeto inicial

---
+++ spotify-react/src/App.js
@@ -0,0 +1,9 @@
+import './App.css'
+import Header from './Header/header'
+import Sidebar from './Sidebar/sidebar'
+
+function App() {
+  return (<Header></Header>), (<Sidebar></Sidebar>)
+}
+
+export default App
